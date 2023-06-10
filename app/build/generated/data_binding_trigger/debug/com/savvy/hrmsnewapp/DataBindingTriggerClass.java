eca72322f5b7576ec2aeeb84cfb15b8977d1b73d
HP <mcp.skgupta@gmail.com>
2023-06-10 14:55:28 +0530

    10 June 2023 Hierarchy by Jilendra Singh

---
+++ app/build/generated/data_binding_trigger/debug/com/savvy/hrmsnewapp/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.savvy.hrmsnewapp;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ app/build/generated/source/buildConfig/release/com/savvy/hrmsnewapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.savvy.hrmsnewapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.savvy.hrmsnewapp";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 52;
+  public static final String VERSION_NAME = "6.6";
+}
